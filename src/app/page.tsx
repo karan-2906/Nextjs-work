0aa5cb9b1b931a3fe687a26a721bc880425e1ce6
karan-2906 <karangandhi486@gmail.com>
2024-05-01 10:27:48 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
 'use client';
 import React from 'react';
 import { AppProps } from 'next/app';
-import { DarkModeProvider } from '../components/darkmode/useDarkMode';
+
 import ProductsPage from '../app/products/page';
-import Navbar from '../components/navbar/navbar';
-// import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-    // <DarkModeProvider>
-      {/* <Component {...pageProps} /> */}
-      <ProductsPage />
-    // </DarkModeProvider>
+        {/* <Component {...pageProps} /> */}
+        <ProductsPage />
     </div>
   );
 }
